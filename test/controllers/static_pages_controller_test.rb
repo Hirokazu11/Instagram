616a891e5ae19e5572888778d2bda06a7f25e642
hirokazu <h.kuwabara1103@gmail.com>
2020-05-04 08:51:58 +0000

    ユーザー登録ページ作成

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup　　 
+
+  def setup
     @base_title = "Instagram"
   end
 
-  
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select 'title', @base_title
+    assert_select "title", @base_title
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select 'title', "Help | Instagram"
+    assert_select "title", "Help | #{@base_title}"
   end
-  
+
 end
