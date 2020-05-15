7ba70a7d71373065cd90eca588ec4d4fd48e5ad4
hirokazu <h.kuwabara1103@gmail.com>
2020-05-15 15:18:45 +0000

    モーダルにコメントも実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page]).search(params[:search])
+    @comment = Comment.new
   end 
  
   def new
