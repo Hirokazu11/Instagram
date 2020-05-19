e2d1484e5f4d2977f4aa0f1c239eac16d02e2372
hirokazu <h.kuwabara1103@gmail.com>
2020-05-19 13:52:52 +0000

    S3セットアップ

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
+31.times do |n|
   name  = Faker::Name.name
-  email = "example#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                user_name: n,
@@ -22,7 +22,7 @@ end
 
 #マイクロポスト
 users = User.order(:created_at).take(6)
-10.times do
+6.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(
                   content: content, 
@@ -32,7 +32,7 @@ end
 #リレーションシップ
 users = User.all
 user = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..10]
+followers = users[3..6]
 following.each { |followed|user.follow(followed) }
 followers.each { |follower|follower.follow(user) }
