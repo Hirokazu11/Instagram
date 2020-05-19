66906bdb70fe146a69e7c77ba81b6a321de86b0c
hirokazu <h.kuwabara1103@gmail.com>
2020-05-19 12:03:08 +0000

    Sendgrid設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                user_name: n,
@@ -22,7 +22,7 @@ end
 
 #マイクロポスト
 users = User.order(:created_at).take(6)
-50.times do
+10.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(
                   content: content, 
