a14f7c1de406b987d47490cfa791362fa2c9dfc1
Hirokazu <h.kuwabara1103@gmail.com>
2020-06-25 21:25:38 +0900

    アカウント認証とdevice不具合のため5月16日のコミット地点まで遡る

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.create!(name:  "Example User",
              password_confirmation: "foobar",
              admin: true)
 
-99.times do |n|
+31.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -18,7 +18,7 @@ end
 
 #マイクロポスト
 users = User.order(:created_at).take(6)
-50.times do
+6.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(
                   content: content, 
@@ -28,7 +28,7 @@ end
 #リレーションシップ
 users = User.all
 user = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..20]
+followers = users[3..10]
 following.each { |followed|user.follow(followed) }
 followers.each { |follower|follower.follow(user) }
