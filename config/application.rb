04558e37b215f316cf8a4b93fea332051c526bb7
Hirokazu <h.kuwabara1103@gmail.com>
2020-07-03 23:57:51 +0900

    heroku コンパイルエラーの対処

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,10 @@ Bundler.require(*Rails.groups)
 module InstaApp
   class Application < Rails::Application
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
       g.test_framework :rspec,
       view_specs: false,
       helper_specs: false,
